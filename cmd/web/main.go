4a3a3ea0e82eebe2fa6385fea010d706fbe08985
Fredrik Siemund <fredrik.siemund@gmail.com>
2022-03-26 11:59:49 +0100

    Setup postgres

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/jackc/pgx/v4"
 )
 
 type application struct {
@@ -15,11 +18,19 @@ type application struct {
 func main() {
 	// Parsing the runtime configuration settings for the application
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	connStr := flag.String("connStr", "postgres://root:root@localhost:5432/dev", "PostgreSQL connection string")
 
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// Establish DB connection
+	db, err := openDb(*connStr)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close(context.Background())
 
 	// Establishing the dependencies for the handlers (depenency injection)
 	app := &application{
@@ -34,8 +45,21 @@ func main() {
 		Handler:  app.routes(),
 	}
 	infoLog.Printf("Starting server on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		errorLog.Fatal(err)
 	}
+}
+
+func openDb(connStr string) (*pgx.Conn, error) {
+	conn, err := pgx.Connect(context.Background(), connStr)
+	if err != nil {
+		return nil, err
+	}
+	// Since connections to the database are established lazily,
+	// we can verify that everything is set up correctly by calling db.Ping()
+	if err = conn.Ping(context.Background()); err != nil {
+		return nil, err
+	}
+	return conn, nil
 }
